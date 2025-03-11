3e0f47ca7301832f93cc8ed5a8fd9f4e1d1d85db
Andrew Nelson <evildarkarchon@gmail.com>
2025-03-10 23:59:32 -0700

    Revert usage of ScanGame hash function because of import issues. Update CLASSIC Fallout4.yaml.sha256

--- CLASSIC Data/classic-fallout4-hashgen.py
+++ CLASSIC Data/classic-fallout4-hashgen.py
@@ -1,10 +1,6 @@
-﻿from pathlib import Path
+﻿import hashlib
+from pathlib import Path
 
-from CLASSIC_ScanGame import calculate_file_hash
-
-data_dir = Path(__file__).parent
-
-Path(f"{data_dir}/databases/CLASSIC Fallout4.yaml.sha256").write_text(calculate_file_hash(Path(f"{data_dir}/databases/CLASSIC Fallout4.yaml")))
-"""with Path(f"{Path(__file__).parent}/databases/CLASSIC Fallout4.yaml").open("rb") as f:
+with Path(f"{Path(__file__).parent}/databases/CLASSIC Fallout4.yaml").open("rb") as f:
     data = f.read()
-    Path(f"{Path(__file__).parent}/databases/CLASSIC Fallout4.yaml.sha256").write_text(hashlib.sha256(data).hexdigest())"""
+    Path(f"{Path(__file__).parent}/databases/CLASSIC Fallout4.yaml.sha256").write_text(hashlib.sha256(data).hexdigest())
