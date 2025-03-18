fecde8619e02c1d111495b61f79b69b91a0344c5
Andrew Nelson <evildarkarchon@gmail.com>
2025-03-17 22:22:58 -0700

    Add comment about Path line.

--- CLASSIC_ScanGame.py
+++ CLASSIC_ScanGame.py
@@ -451,7 +451,7 @@ def check_crashgen_settings() -> str:
     """
     message_list: list[str] = []
     plugins_path = CMain.yaml_settings(Path, CMain.YAML.Game_Local, f"Game{CMain.gamevars['vr']}_Info.Game_Folder_Plugins")
-    plugins_path = Path(plugins_path) if plugins_path and not isinstance(plugins_path, Path) else plugins_path
+    plugins_path = Path(plugins_path) if plugins_path and not isinstance(plugins_path, Path) else plugins_path  # Explicilitly cast to Path if not already.
     crashgen_name_setting = CMain.yaml_settings(str, CMain.YAML.Game, f"Game{CMain.gamevars['vr']}_Info.CRASHGEN_LogName")
     crashgen_name = crashgen_name_setting if isinstance(crashgen_name_setting, str) else ""
 
