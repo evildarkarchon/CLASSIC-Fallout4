8743c9b5f21b8f839998c9abe601019bb286110a
Andrew Nelson <evildarkarchon@gmail.com>
2025-06-12 23:21:19 -0700

    Expand `__init__.py` imports across `ClassicLib` modules to include all relevant components, ensuring improved accessibility and maintainability.

--- ClassicLib/ScanLog/__init__.py
+++ ClassicLib/ScanLog/__init__.py
@@ -4,6 +4,20 @@ ScanLog package initialization.
 This package contains modules for scanning and analyzing crash logs.
 """
 
+# Core scanning components
+# Async components
+from ClassicLib.ScanLog.AsyncFileIO import integrate_async_file_loading
+from ClassicLib.ScanLog.AsyncFormIDAnalyzer import AsyncFormIDAnalyzer
+from ClassicLib.ScanLog.AsyncIntegration import run_async_scan
+from ClassicLib.ScanLog.AsyncPipeline import AsyncCrashLogPipeline
+from ClassicLib.ScanLog.AsyncReformat import (
+    batch_file_copy_async,
+    batch_file_move_async,
+    crashlogs_reformat_async,
+    reformat_single_log_async,
+)
+from ClassicLib.ScanLog.AsyncScanOrchestrator import AsyncScanOrchestrator, write_reports_batch_async
+from ClassicLib.ScanLog.AsyncUtil import AsyncDatabasePool, write_file_async
 from ClassicLib.ScanLog.DetectMods import detect_mods_double, detect_mods_important, detect_mods_single
 from ClassicLib.ScanLog.FCXModeHandler import FCXModeHandler
 from ClassicLib.ScanLog.FormIDAnalyzer import FormIDAnalyzer
@@ -16,9 +30,26 @@ from ClassicLib.ScanLog.ScanLogInfo import ClassicScanLogsInfo, ThreadSafeLogCac
 from ClassicLib.ScanLog.ScanOrchestrator import ScanOrchestrator
 from ClassicLib.ScanLog.SettingsScanner import SettingsScanner
 from ClassicLib.ScanLog.SuspectScanner import SuspectScanner
-from ClassicLib.ScanLog.Util import crashlogs_get_files, crashlogs_reformat, get_entry
+
+# Utility functions
+from ClassicLib.ScanLog.Util import (
+    copy_files,
+    crashlogs_get_files,
+    crashlogs_reformat,
+    ensure_directory_exists,
+    get_entry,
+    get_path_from_setting,
+    is_valid_custom_scan_path,
+    move_files,
+)
 
 __all__ = [
+    # Async components
+    "AsyncCrashLogPipeline",
+    "AsyncDatabasePool",
+    "AsyncFormIDAnalyzer",
+    "AsyncScanOrchestrator",
+    # Core components
     "ClassicScanLogsInfo",
     "FCXModeHandler",
     "FormIDAnalyzer",
@@ -29,16 +60,33 @@ __all__ = [
     "SettingsScanner",
     "SuspectScanner",
     "ThreadSafeLogCache",
+    "batch_file_copy_async",
+    "batch_file_move_async",
+    # Utility functions
+    "copy_files",
     "crashlogs_get_files",
     "crashlogs_reformat",
+    "crashlogs_reformat_async",
+    # Detection functions
     "detect_mods_double",
     "detect_mods_important",
     "detect_mods_single",
+    "ensure_directory_exists",
+    # Parser functions
     "extract_module_names",
     "extract_segments",
     "find_segments",
     "get_entry",
+    # GPU functions
     "get_gpu_info",
+    "get_path_from_setting",
+    "integrate_async_file_loading",
+    "is_valid_custom_scan_path",
+    "move_files",
     "parse_crash_header",
+    "reformat_single_log_async",
+    "run_async_scan",
     "scan_log_gpu",
+    "write_file_async",
+    "write_reports_batch_async",
 ]
--- ClassicLib/__init__.py
+++ ClassicLib/__init__.py
@@ -1,10 +1,39 @@
 """ClassicLib - Core library for CLASSIC crash log analyzer."""
 
-from ClassicLib.Constants import NG_VERSION, NULL_VERSION, OG_VERSION, YAML, GameID
-from ClassicLib.GlobalRegistry import Keys, get, get_game, get_vr, register
+from ClassicLib.Constants import (
+    DB_PATHS,
+    F4SE_VERSIONS,
+    FO4_VERSIONS,
+    NG_F4SE_VERSION,
+    NG_VERSION,
+    NULL_VERSION,
+    OG_F4SE_VERSION,
+    OG_VERSION,
+    SETTINGS_IGNORE_NONE,
+    VR_VERSION,
+    YAML,
+    GameID,
+)
+from ClassicLib.GlobalRegistry import (
+    Keys,
+    get,
+    get_game,
+    get_game_path_gui,
+    get_local_dir,
+    get_manual_docs_gui,
+    get_vr,
+    get_yaml_cache,
+    is_gui_mode,
+    is_registered,
+    register,
+)
+from ClassicLib.Logger import logger
 from ClassicLib.MessageHandler import (
+    Message,
+    MessageHandler,
     MessageTarget,
     MessageType,
+    ProgressContext,
     get_message_handler,
     init_message_handler,
     msg_critical,
@@ -15,26 +44,99 @@ from ClassicLib.MessageHandler import (
     msg_success,
     msg_warning,
 )
-from ClassicLib.Util import calculate_file_hash, open_file_with_encoding
-from ClassicLib.YamlSettingsCache import YamlSettingsCache, classic_settings, yaml_settings
+from ClassicLib.Meta import SingletonMeta
+from ClassicLib.Update import (
+    UpdateCheckError,
+    get_github_latest_stable_version_from_endpoint,
+    get_latest_and_top_release_details,
+    get_nexus_version,
+    is_latest_version,
+    try_parse_version,
+)
+from ClassicLib.Util import (
+    append_or_extend,
+    calculate_file_hash,
+    calculate_similarity,
+    configure_logging,
+    crashgen_version_gen,
+    get_game_version,
+    normalize_list,
+    open_file_with_encoding,
+    pastebin_fetch,
+    pastebin_fetch_async,
+    remove_readonly,
+)
+from ClassicLib.XseCheck import xse_check_hashes, xse_check_integrity
+from ClassicLib.YamlSettingsCache import (
+    YAMLLiteral,
+    YAMLMapping,
+    YAMLSequence,
+    YamlSettingsCache,
+    YAMLValue,
+    YAMLValueOptional,
+    classic_settings,
+    yaml_cache,
+    yaml_settings,
+)
 
 __all__ = [
+    # Constants
+    "DB_PATHS",
+    "F4SE_VERSIONS",
+    "FO4_VERSIONS",
+    "NG_F4SE_VERSION",
     "NG_VERSION",
     "NULL_VERSION",
+    "OG_F4SE_VERSION",
     "OG_VERSION",
+    "SETTINGS_IGNORE_NONE",
+    "VR_VERSION",
     "YAML",
     "GameID",
+    # GlobalRegistry
     "Keys",
+    # MessageHandler
+    "Message",
+    "MessageHandler",
     "MessageTarget",
     "MessageType",
+    "ProgressContext",
+    # Meta
+    "SingletonMeta",
+    # Update
+    "UpdateCheckError",
+    # YamlSettingsCache
+    "YAMLLiteral",
+    "YAMLMapping",
+    "YAMLSequence",
+    "YAMLValue",
+    "YAMLValueOptional",
     "YamlSettingsCache",
+    # Util
+    "append_or_extend",
     "calculate_file_hash",
+    "calculate_similarity",
     "classic_settings",
+    "configure_logging",
+    "crashgen_version_gen",
     "get",
     "get_game",
+    "get_game_path_gui",
+    "get_game_version",
+    "get_github_latest_stable_version_from_endpoint",
+    "get_latest_and_top_release_details",
+    "get_local_dir",
+    "get_manual_docs_gui",
     "get_message_handler",
+    "get_nexus_version",
     "get_vr",
+    "get_yaml_cache",
     "init_message_handler",
+    "is_gui_mode",
+    "is_latest_version",
+    "is_registered",
+    # Logger
+    "logger",
     "msg_critical",
     "msg_debug",
     "msg_error",
@@ -42,7 +144,16 @@ __all__ = [
     "msg_progress_context",
     "msg_success",
     "msg_warning",
+    "normalize_list",
     "open_file_with_encoding",
+    "pastebin_fetch",
+    "pastebin_fetch_async",
     "register",
+    "remove_readonly",
+    "try_parse_version",
+    # XseCheck
+    "xse_check_hashes",
+    "xse_check_integrity",
+    "yaml_cache",
     "yaml_settings",
 ]
